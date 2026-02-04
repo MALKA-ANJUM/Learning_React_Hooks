89258d50cf6d09720c642f597f0527fb7ab061a2
Malka Anjum <malkaanjum1999@gmail.com>
2026-02-04 23:27:57 +0530

    interview question

--- src/Practice/Intreview.jsx
+++ src/Practice/Intreview.jsx
@@ -13,6 +13,9 @@ const Intreview = () => {
 
     return (
         <div>
+            
+            
+
             <p>{count}</p>
             <button onClick={() => setCount(count + 1)}>+</button>
             <button onClick={() => setCount(count - 1)}>-</button>
