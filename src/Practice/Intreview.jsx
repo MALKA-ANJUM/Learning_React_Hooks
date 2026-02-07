c7e76c4e212639ca5abdb20e52c797e85cb8f4d4
Malka Anjum <malkaanjum1999@gmail.com>
2026-02-07 23:18:10 +0530

    changes updated

--- src/Practice/Intreview.jsx
+++ src/Practice/Intreview.jsx
@@ -13,9 +13,6 @@ const Intreview = () => {
 
     return (
         <div>
-            
-            
-
             <p>{count}</p>
             <button onClick={() => setCount(count + 1)}>+</button>
             <button onClick={() => setCount(count - 1)}>-</button>
