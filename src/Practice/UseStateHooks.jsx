fbe0e4cc4f3390285c651f8100f5c06290a6bf78
Malka Anjum <malkaanjum1999@gmail.com>
2026-02-11 22:40:30 +0530

    print all odd numbers between 1 to 100

--- src/Practice/UseStateHooks.jsx
+++ src/Practice/UseStateHooks.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 const UseStateHooks = () => {
+    // print all odd numbers between 1 to 100
+    for(let x=1; x<=100; x++){
+        if(x%2 != 0){
+            console.log(x);
+        }
+    }
     return (
         <div>
-            
+           
         </div>
     )
 }
