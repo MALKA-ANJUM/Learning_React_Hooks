b7b69889a37037c08d6957c186d60ec8d3c97c63
Malka Anjum <malkaanjum1999@gmail.com>
2026-02-15 22:21:26 +0530

    counter added without state

--- src/Practice/UseStateHooks.jsx
+++ src/Practice/UseStateHooks.jsx
@@ -7,9 +7,15 @@ const UseStateHooks = () => {
             console.log(x);
         }
     }
+    let counter = 0;
+    const increment = () => {
+        counter = counter + 1;
+        console.log(counter);
+    }
     return (
         <div>
-           
+            {counter}
+            <button onClick={increment}>Increment</button>
         </div>
     )
 }
